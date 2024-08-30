17f5e7235d29dc38f01edc2b9548431f8c2ef1ff
zoukihub <moraabochere@gmail.com>
2024-08-30 12:17:19 +0300

    Stack 1 - enqueue

--- hackathon.py
+++ hackathon.py
@@ -1,10 +1,15 @@
 # 1. Reverse a string using a stack
-def reverse_string(s: str) -> str:
-    stack = []
-    for char in s:
-        stack.append(char)
-    reversed_string = ''.join(stack.pop() for _ in range(len(stack)))
+# def reverse_string(s: str) -> str:
+#     stack = []
+#     for char in s:
+#         stack.append(char)
+#     reversed_string = ''.join(stack.pop() for _ in range(len(stack)))
 
-    return reversed_string
-print(reverse_string("hello"))
+#     return reversed_string
+# print(reverse_string("hello"))
 
+# 2. Implement a Queue Using Two Stacks
+class QueueWithStacks:
+    def__init__(self):
+        self.stack1 = []
+ 
