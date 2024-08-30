17140323444b801e9a180540b856f05399112fba
zoukihub <moraabochere@gmail.com>
2024-08-30 13:35:29 +0300

    Move from Stack1-Stack2

--- hackathon.py
+++ hackathon.py
@@ -16,5 +16,8 @@ class QueueWithStacks:
 
     def enqueue(self, x: int):
         self.stack1.append(x)
-        
- 
+
+    def dequeue(self) -> int:
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
