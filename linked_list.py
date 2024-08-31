2920ae50d466adef33afcf880efbb25f85c13c3c
Itumeleng Mphuti <125954886+ItumelengMphuti@users.noreply.github.com>
2024-08-31 19:15:11 +0200

    Finding the max. element in a list using a linked list

---
+++ linked_list.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def find_max(self) -> int:
+        if self.head is None:
+            return None  
+
+        max_value = self.head.data
+        current = self.head.next
+
+        while current:
+            if current.data > max_value:
+                max_value = current.data
+            current = current.next
+
+        return max_value
+
+
+ll = LinkedList()
+ll.append(3)
+ll.append(1)
+ll.append(4)
+ll.append(2)
+ll.append(5)
+print(ll.find_max()) 
